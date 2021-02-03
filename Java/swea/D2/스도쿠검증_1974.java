097b60b8c040ba23d74e82c7bef2ce47f9a3b316
SPJ <52192543+pyo7410@users.noreply.github.com>
2021-02-03 14:41:50 +0900

    Update 스도쿠검증_1974.java

--- Java/swea/D2/스도쿠검증_1974.java
+++ Java/swea/D2/스도쿠검증_1974.java
@@ -1,96 +1,87 @@
-import java.util.Scanner;
+package D2;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class 스도쿠검증_1974 {
 	
-	static int[][] arr;
-	static boolean[] isUsed;
+	static int[][] sudoku;
+	static HashSet<Integer> set = new HashSet<Integer>();
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder("");
 		
-		int t = sc.nextInt();
+		int t = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= t; ++tc) {
-			arr = new int[9][9];
+			sudoku = new int[9][9];
 			
 			for (int i = 0; i < 9; ++i) {
+				String input = br.readLine();
+				StringTokenizer st = new StringTokenizer(input, " ");
 				for (int j = 0; j < 9; ++j) {
-					arr[i][j] = sc.nextInt();
+					sudoku[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			// 3x3 체크
-			int check1 = 1;
-			for (int i = 0; i < 9; i += 3) {
-				if (check1 == 0) {
-					break;
-				}
-				
-				for (int j = 0; j < 9; j += 3) {
-					if (check1 == 0) {
-						break;
-					}
-					
-					isUsed = new boolean[10];
-					
-					check1 = check3x3(i, j);
-				}
-			}
+			int answer = (check3x3() && checkRow() && checkColumn()) ? 1 : 0;
 			
-			// 가로, 세로 체크
-			int check2 = 1;
-			for (int i = 0; i < 9; ++i) {
-				if (check2 == 0) {
-					break;
-				}
-				
-				isUsed = new boolean[10];
-				
-				for (int j = 0; j < 9; ++j) {
-					if (isUsed[arr[i][j]]) {
-						check2 = 0;
-						break;
-					}					
-					
-					isUsed[arr[i][j]] = true;
-				}
-			}
-			
-			int check3 = 1;
-			for (int i = 0; i < 9; ++i) {
-				if (check3 == 0) {
-					break;
-				}
-				
-				isUsed = new boolean[10];
-				
-				for (int j = 0; j < 9; ++j) {					
-					if (isUsed[arr[j][i]]) {
-						check3 = 0;
-						break;
-					}
-					
-					isUsed[arr[j][i]] = true;
-				}
-			}
-			
-			System.out.println("#" + tc + " " + ((check1 == 1 && check2 == 1 && check3 == 1) ? 1 : 0));
+			sb.append("#").append(tc).append(" ").append(answer);
+			System.out.println(sb);
+			sb.setLength(0);
 		}
-		
-		sc.close();
 	}
 	
-	public static int check3x3(int i, int j) {
-		for (int k = 0; k < 3; ++k) {
-			for (int l = 0; l < 3; ++l) {
-				if (isUsed[arr[i + k][j + l]]) {
-					return 0;
+	public static boolean check3x3() {
+		for (int i = 0; i < 9; i += 3) {
+			for (int j = 0; j < 9; j += 3) {
+				set.clear();
+					
+				for (int ii = 0; ii < 3; ++ii) {
+					for (int jj = 0; jj < 3; ++jj) {
+						set.add(sudoku[i + ii][j + jj]);
+					}
 				}
 				
-				isUsed[arr[i + k][j + l]] = true;
+				if (set.size() < 9) {
+					return false;
+				}
 			}
 		}
 		
-		return 1;
+		return true;
 	}
-
+	
+	public static boolean checkRow() {
+		for (int i = 0; i < 9; ++i) {
+			set.clear();
+			
+			for (int j = 0; j < 9; ++j) {
+				set.add(sudoku[i][j]);
+			}
+			
+			if (set.size() < 9) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static boolean checkColumn() {
+		for (int i = 0; i < 9; ++i) {
+			set.clear();
+			
+			for (int j = 0; j < 9; ++j) {
+				set.add(sudoku[j][i]);
+			}
+			
+			if (set.size() < 9) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
