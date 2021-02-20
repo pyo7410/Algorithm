b96e9aa3f687fc74d68a7698625847e4745a789e
SPJ <52192543+pyo7410@users.noreply.github.com>
2021-02-20 21:34:40 +0900

    add: Programmers 문제 추가(Python)
    - K번째 수(Level 1)

---
+++ Python/Programmers/level 1/K번째수.py
@@ -0,0 +1,12 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        slice_arr = array[command[0] - 1:command[1]]
+        slice_arr.sort()
+        answer.append(slice_arr[command[2] - 1])
+
+    return answer
+
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
