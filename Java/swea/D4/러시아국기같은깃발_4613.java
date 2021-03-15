0f61b4f0de439efd6277b2ee84be2461b54cda01
SPJ <52192543+pyo7410@users.noreply.github.com>
2021-03-15 15:21:52 +0900

    add: SWEA 문제 추가(Java)
    - 4613번 러시아 국기 같은 깃발(D4)

---
+++ Java/swea/D4/러시아국기같은깃발_4613.java
@@ -0,0 +1,93 @@
+package D4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 러시아국기같은깃발_4613 {
+	public static int N, M, answer;
+	public static char[][] flag;
+	public static char[] colors;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder("");
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; ++tc) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			flag = new char[N][M];
+			colors = new char[N];
+			
+			for (int i = 0; i < N; ++i) {
+				flag[i] = br.readLine().toCharArray();
+			}
+			
+			answer = 987654321;
+			
+			go(0, 'W');
+			
+			for (int i = 0; i < M; ++i) {
+				if (flag[0][i] != 'W') {
+					answer++;
+				}
+				if (flag[N - 1][i] != 'R') {
+					answer++;
+				}
+			}
+			
+			
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+	
+	public static void go(int cnt, char cur_color) {
+		// 맨 마지막과 맨 처음은 항상 하얀색과 빨간색!
+		if (cnt == N - 2) {
+			int maxColorCnt = 0;
+			for (int i = 1; i < N - 1; ++i) {
+				int colorCnt = 0;
+				
+				for (int j = 0; j < M; ++j) {
+					if (flag[i][j] != colors[i - 1]) {
+						colorCnt++;
+					}
+				}
+				
+				maxColorCnt += colorCnt;
+			}
+			
+			answer = (answer > maxColorCnt) ? maxColorCnt : answer;
+			return;
+		}
+		
+		if (cnt == N - 3 && cur_color == 'W') {
+			colors[cnt] = 'B';
+			go(cnt + 1, 'B');
+			return;
+		}
+		
+		if (cur_color == 'W') {
+			colors[cnt] = 'W';
+			go(cnt + 1, 'W');
+			colors[cnt] = 'B';
+			go(cnt + 1, 'B');
+		}
+		else if (cur_color == 'B') {
+			colors[cnt] = 'B';
+			go(cnt + 1, 'B');
+			colors[cnt] = 'R';
+			go(cnt + 1, 'R');
+		}
+		else {
+			colors[cnt] = 'R';
+			go(cnt + 1, 'R');
+		}
+	}
+}
